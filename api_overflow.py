7ceb3294497715782f2875e8c16269d60d299b45
Oscar Cuellas Cuellas <e.gvp51.tid@telefonica.com>
2021-11-24 13:42:15 +0100

    Update api_overflow.py

--- api_overflow.py
+++ api_overflow.py
@@ -197,7 +197,9 @@ class Data_input:
         try:
             with open(path_bucket_file) as file:
                 for line in file:
-                    self.__list_buckets.append(Bucket(str(line), type, connect)))
+                
+		    buketId,name,hosts = line.split()
+                    self.__list_buckets.append(Bucket(bucketId, type, connect)))
 
             return self.__list_buckets      
             logger.info('Data_input.secrets(): Load bucket lists on file: '.format(path_secrets_json_file))
